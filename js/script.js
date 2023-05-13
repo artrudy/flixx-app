5d90814702590801417e4339024d9eb2ec44b7ef
artrudy <art.rudy@gmail.com>
2023-05-13 22:22:01 +0300

    movie picture fetching added

--- js/script.js
+++ js/script.js
@@ -2,24 +2,79 @@ const global = {
     currentPage: window.location.pathname,
 }
 
+async function displayPopularMovies() {
+    const {results} = await fetchAPIData('movie/popular');
+    // console.log(results);    
+    results.forEach(movie => {
+        const div = document.createElement('div');
+        div.classList.add('card');
+        div.innerHTML = `
+        <a href="movie-details.html?id=${movie.id}">
+          ${
+            movie.poster_path
+            ?` <img
+            src="https://image.tmdb.org/t/p/w500${movie.poster_path}"
+            class="card-img-top"
+            alt="${movie.title}"
+          />` :
+          `
+          <img
+          src="images/no-image.jpg"
+          class="card-img-top"
+          alt="${movie.title}"
+        />
+          `
+
+          }
+        </a>
+        <div class="card-body">
+          <h5 class="card-title">${movie.title}</h5>
+          <p class="card-text">
+            <small class="text-muted">Release: ${movie.release_date}</small>
+          </p>
+        </div>
+    `;
+
+    document.querySelector('#popular-movies').appendChild(div)
+    })
+}
+
+//Fetching data from TMDB API
+
+async function fetchAPIData(endpoint) {
+    const API_KEY = 'd46a260bcd3d09bb7a83480819d5c14e';
+    const API_URL = 'https://api.themoviedb.org/3/';
+
+    const response = await fetch(`${API_URL}${endpoint}?api_key=${API_KEY}&languaage=by-BY`);
+    const data = await response.json();
+    return data;    
+}
+
+function showSpinner() {
+    document.querySelector('spinner').classList.add('show');    
+}
+
+function hideSpinner() {
+    document.querySelector('spinner').classList.remove('show');    
+}
+
 //highlight active link
 function highlightActiveLink () {
     const links = document.querySelectorAll('.nav-link');
     links.forEach((link) => {
         if(link.getAttribute('href') === global.currentPage){
             link.classList.add('active');
-
         }
     })
 }
-
 
 //init app
 function init(){
     switch(global.currentPage) {
         case '/':
         case '/index.html':
-            console.log('Home');
+            displayPopularMovies();
+            // console.log('Home');
             break;
 
         case '/shows.html':
@@ -34,7 +89,6 @@ function init(){
         case '/search.httml':
             console.log('Search');
             break;
-
     }
     highlightActiveLink();
 }
